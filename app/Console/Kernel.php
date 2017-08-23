8ed95dbff078c75575ed778e9e76f1cb43d8ac0f
abhishek wani <abhishek wani>
2017-08-23 15:32:05 +0530

    First Commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('coupomated:fetch --force')->daily();
+        $schedule->command('coupomated:fetch')->hourly();
     }
 
     /**
