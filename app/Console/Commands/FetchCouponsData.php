f9d78ceb8e4ceab85c4cf7ea5fe7f02f8b69f392
abhishek wani <abhishek wani>
2017-09-02 14:56:34 +0530

    Coupons Search Api

--- app/Console/Commands/FetchCouponsData.php
+++ app/Console/Commands/FetchCouponsData.php
@@ -59,10 +59,14 @@ class FetchCouponsData extends Command
                 } 
                 
                  $pieces = parse_url($coupon->LINK);
-                
-                    $logo_path = "https://logo.clearbit.com/" + $pieces['host'];
+                  $domain = isset($pieces['host']) ? $pieces['host'] : '';
+                  if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs)) {
+                    $logo_path = "https://logo.clearbit.com/" . $regs['domain'];
                     
-                 
+                  } else {
+                     $logo_path = '';
+                  }
+              
                 
                 
                 Coupon::create([
