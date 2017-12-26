a233d4069d66dd1c12f157acc09582e29e55d686
abhishek47 <abhishekwani@hotmail.com>
2017-12-26 19:54:32 +0530

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('coupomated:fetch')->nightly();
+        $schedule->command('coupomated:fetch')->daily();
     }
 
     /**
